2f4e440694676202052987db55ae7c14d3c75df6
jheimisson <jheimissonsantos100@gmail.com>
2024-10-31 22:47:39 -0300

    projeto finalizado

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -15,18 +17,25 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            
+            try
+            { 
+                if (hospedes.Count<= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+           
+             else
+             {
+                throw new Exception("o numero de hospedes excede a capacidade da suite");
+             }
             }
-        }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            }
 
         public void CadastrarSuite(Suite suite)
         {
@@ -35,23 +44,22 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            
+            Console.WriteLine($"a quantidade de hospedes é {Hospedes.Count}");
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            
+            decimal valor =  DiasReservados*Suite.ValorDiaria;
+           
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados>=10)
             {
-                valor = 0;
+               valor=valor-(valor * 10/100);
+               Console.WriteLine($"seu desconto é 10%");
+                
             }
 
             return valor;
